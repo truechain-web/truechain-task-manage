5f2115aea8c6cb290c1edbff93d8b03b6a1262f6
addinGao <1516832932@qq.com>
2018-07-12 12:42:20 +0800

    系统管理+配置管理界面完成

--- src/components/page/DataStatis.vue
+++ src/components/page/DataStatis.vue
@@ -1,17 +1,141 @@
 <template>
- <div>
-	  数据统计
+ <div class="datastatis">
+	  <div class="top">
+				<div>
+						<span style="line-height:36px;">姓名：</span>
+						<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+						<span style="line-height:36px;">微信名称：</span>
+						<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+						<span style="line-height:36px;">等级：</span>
+						<el-select v-model="optionsValue" placeholder="全部">
+									<el-option
+										v-for="item in options"
+										:key="item.value"
+										:label="item.label"
+										:value="item.value">
+									</el-option>
+						</el-select>
+					
+						<el-button type="primary">查询</el-button>
+						<el-button type="warning">重置</el-button>
+				</div>
+				<el-button type="success" style="width:100px">新增按钮</el-button>
+	 </div>
+	 <div class="data-table">
+			<el-table
+				:data="tableData"
+				border
+				style="width: 100%">
+				<el-table-column  prop="name"	label="姓名"></el-table-column>
+				<el-table-column prop="wechatname"	label="微信昵称"></el-table-column>
+				<el-table-column	prop="wechatid"	label="微信号"></el-table-column>
+				<el-table-column	prop="task"	label="抢任务数"></el-table-column>
+				<el-table-column	prop="tasked"	label="完成任务数"></el-table-column>
+				<el-table-column	prop="tasking"	label="进行中任务数"></el-table-column>
+				<el-table-column	prop="truenum"	label="true数量"></el-table-column>
+				<el-table-column	prop="ttrnum"	label="ttr数量"></el-table-column>
+				<el-table-column	prop="rmbnum"	label="rmb数量"></el-table-column>
+				<el-table-column	prop="recommendnum"	label="用户推荐数"></el-table-column>
+				<el-table-column	label="操作">
+							<template slot-scope="scope">
+								<el-button size="mini"	
+									@click="handleEdit(scope.$index, scope.row)">任务列表</el-button>
+								<el-button size="mini"	
+									@click="handleEdit(scope.$index, scope.row)">推荐列表</el-button>
+								<el-button size="mini"
+									@click="handleDelete(scope.$index, scope.row)">奖励列表</el-button>
+							</template>
+				</el-table-column>
+			</el-table>
+			<div class="page">
+					<el-pagination background layout="prev, pager, next"	:total="1000">	</el-pagination>
+			</div>
+  	</div>
  </div>
 </template>
 <script>
  export default {
 	  data(){
 			return {
-				 
+				tableData:[{
+          name: '王小虎',
+          wechatname: 'ai',
+          wechatid: 'Gao',
+					task:10,
+					tasked:23,
+					tasking:44,
+					truenum:33,
+					ttrnum:22,
+					rmbnum:333,
+					recommendnum:55
+        },{
+          name: '王小虎',
+          wechatname: 'ai',
+          wechatid: 'Gao',
+					task:10,
+					tasked:23,
+					tasking:44,
+					truenum:33,
+					ttrnum:22,
+					rmbnum:333,
+					recommendnum:55
+        },{
+          name: '王小虎',
+          wechatname: 'ai',
+          wechatid: 'Gao',
+					task:10,
+					tasked:23,
+					tasking:44,
+					truenum:33,
+					ttrnum:22,
+					rmbnum:333,
+					recommendnum:55
+        },{
+          name: '王小虎',
+          wechatname: 'ai',
+          wechatid: 'Gao',
+					task:10,
+					tasked:23,
+					tasking:44,
+					truenum:33,
+					ttrnum:22,
+					rmbnum:333,
+					recommendnum:55
+        }],
+				 options: [{
+          value: '选项1',
+          label: '全部'
+        }, {
+          value: '选项2',
+          label: 'A'
+        }, {
+          value: '选项3',
+          label: 'B'
+        },
+				{
+          value: '选项4',
+          label: 'C'
+        }],
+				optionsValue:""
 			}
 		}
  }
 </script>	
-<style scoped>
-
+<style scoped lang="less">
+.datastatis{
+  .top{
+		  display: flex;
+			height: 36px;
+			align-content: center;
+			justify-content: space-between;
+	}
+	.data-table{
+		 margin-top: 20px;
+		.page{
+				display: flex;
+				justify-content: center;
+				margin-top: 20px;
+		}
+	}
+}
 </style>
--- src/components/page/SystemManage.vue
+++ src/components/page/SystemManage.vue
@@ -5,7 +5,7 @@
 				<el-tab-pane label="权限管理" name="first">
 					  <div class="quanxian">
 							  <div>
-									<span style="line-height:36px;">权限名称</span>
+									<span style="line-height:36px;">权限名称：</span>
 									<el-input placeholder="请输入内容" style="width:300px; margin: 0 20px;"></el-input>
 									<el-button type="primary">查询</el-button>
 									<el-button type="warning">重置</el-button>
@@ -39,9 +39,147 @@
 						
 						</div>
 				</el-tab-pane>
-				<el-tab-pane label="角色管理" name="second">角色管理</el-tab-pane>
-				<el-tab-pane label="用户管理" name="third">用户管理</el-tab-pane>
-				<el-tab-pane label="日志" name="fourth">日志</el-tab-pane>
+
+				<!-- 角色管理部分 -->
+				<el-tab-pane label="角色管理" name="second">
+					  <div class="jiaose">
+							  <div>
+									<span style="line-height:36px;">角色名称：</span>
+									<el-input placeholder="请输入内容" style="width:150px; margin: 0 20px;"></el-input>
+									<span style="line-height:36px;">角色状态：</span>
+									<el-select v-model="optionsValue" placeholder="全部">
+												<el-option
+													v-for="item in options"
+													:key="item.value"
+													:label="item.label"
+													:value="item.value">
+												</el-option>
+									</el-select>
+									<el-button type="primary">查询</el-button>
+									<el-button type="warning">重置</el-button>
+								</div>
+								<el-button type="success" style="width:100px">新增按钮</el-button>
+						</div>
+						<div class="quanxian-table">
+							  <el-table
+									:data="tableData"
+									border
+									style="width: 100%">
+									<el-table-column  prop="date"	label="角色编号"></el-table-column>
+									<el-table-column prop="name"	label="角色名称"></el-table-column>
+									<el-table-column	prop="address"	label="角色状态"></el-table-column>
+									<el-table-column label="描述"></el-table-column>
+									<el-table-column	label="操作">
+										    <template slot-scope="scope">
+													<el-button size="mini"
+														@click="handleEdit(scope.$index, scope.row)" type="primary">查看详情</el-button>
+													<el-button size="mini"	
+														@click="handleEdit(scope.$index, scope.row)">修改</el-button>
+													<el-button size="mini"
+														type="danger"
+														@click="handleDelete(scope.$index, scope.row)">删除</el-button>
+												</template>
+									</el-table-column>
+								</el-table>
+								<div class="page">
+										<el-pagination background layout="prev, pager, next"	:total="1000">	</el-pagination>
+								</div>
+						</div>
+				</el-tab-pane>
+
+				<!-- 用户管理部分 -->
+				<el-tab-pane label="用户管理" name="third">
+					  <div class="yonghu">
+							  <div>
+									<span style="line-height:36px;">登录账号：</span>
+									<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+									<span style="line-height:36px;">姓名：</span>
+									<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+									<span style="line-height:36px;">手机号：</span>
+									<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+									<span style="line-height:36px;">系统角色：</span>
+									<el-input placeholder="请输入内容" style="width:120px; margin: 0 20px;"></el-input>
+									<el-button type="primary">查询</el-button>
+									<el-button type="warning">重置</el-button>
+								</div>
+								<el-button type="success" style="width:100px">新增按钮</el-button>
+						</div>
+						<div class="quanxian-table">
+							  <el-table
+									:data="tableData"
+									border
+									style="width: 100%">
+									<el-table-column  prop="date"	label="登录账号"></el-table-column>
+									<el-table-column prop="name"	label="姓名"></el-table-column>
+									<el-table-column	prop="address"	label="手机号"></el-table-column>
+									<el-table-column label="系统角色"></el-table-column>
+									<el-table-column label="描述"></el-table-column>
+									<el-table-column	label="操作">
+										    <template slot-scope="scope">
+													<el-button size="mini"
+														@click="handleEdit(scope.$index, scope.row)" type="primary">查看详情</el-button>
+													<el-button size="mini"	
+														@click="handleEdit(scope.$index, scope.row)">修改</el-button>
+													<el-button size="mini"
+														type="danger"
+														@click="handleDelete(scope.$index, scope.row)">删除</el-button>
+												</template>
+									</el-table-column>
+								</el-table>
+								<div class="page">
+										<el-pagination background layout="prev, pager, next"	:total="1000">	</el-pagination>
+								</div>
+						
+						</div>
+				</el-tab-pane>
+
+				<!-- 日志部分 -->
+				<el-tab-pane label="日志" name="fourth">
+					  <div class="rizhi">
+							  <div>
+									<span style="line-height:36px;">更新时间：</span>
+									<el-date-picker
+										v-model="begindata"
+										type="date"
+										placeholder="选择日期">
+									</el-date-picker>
+									<span style="line-height:36px;">--</span>
+									<el-date-picker
+										v-model="enddata"
+										type="date"
+										placeholder="选择日期">
+									</el-date-picker>
+									<span style="line-height:36px;margin-left:20px;">更新者：</span>
+									<el-input  style="width:120px; margin: 0 20px;"></el-input>
+									<el-button type="primary">查询</el-button>
+									<el-button type="warning">重置</el-button>
+								</div>
+								<el-button type="success" style="width:100px">新增按钮</el-button>
+						</div>
+						<div class="quanxian-table">
+							  <el-table
+									:data="tableData"
+									border
+									style="width: 100%">
+									<el-table-column  prop="date"	label="日志内容"></el-table-column>
+									<el-table-column prop="name"	label="更新时间"></el-table-column>
+									<el-table-column	prop="address"	label="更新者"></el-table-column>
+									<el-table-column	label="操作">
+										    <template slot-scope="scope">
+													<el-button size="mini"	
+														@click="handleEdit(scope.$index, scope.row)">修改</el-button>
+													<el-button size="mini"
+														type="danger"
+														@click="handleDelete(scope.$index, scope.row)">删除</el-button>
+												</template>
+									</el-table-column>
+								</el-table>
+								<div class="page">
+										<el-pagination background layout="prev, pager, next"	:total="1000">	</el-pagination>
+								</div>
+						
+						</div>
+				</el-tab-pane>
 		</el-tabs>
  </div>
 </template>
@@ -95,7 +233,20 @@
           date: '2016-05-03',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        }],
+				options: [{
+          value: '选项1',
+          label: '全部'
+        }, {
+          value: '选项2',
+          label: '启用'
+        }, {
+          value: '选项3',
+          label: '禁用'
+        }],
+				optionsValue:"",
+			  begindata:'',
+				enddata:''
 			}
 		},
 		methods: {
@@ -111,14 +262,22 @@
 	 height: 36px;
 	 align-content: center;
 	 justify-content: space-between;
+	
 }
 .quanxian-table{
-	 margin-top: 20px;
-	 .page{
+	margin-top: 20px;
+	.page{
 			display: flex;
 			justify-content: center;
 			margin-top: 20px;
-	 }
+	}
 }
+.jiaose,.yonghu,.rizhi{
+	 display: flex;
+	 height: 36px;
+	 align-content: center;
+	 justify-content: space-between;
+}
+
 </style>
 
