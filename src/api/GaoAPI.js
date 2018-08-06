d03a65797fa22499e73c129d1729fca8b8977255
addinGao <1516832932@qq.com>
2018-08-06 09:24:38 +0800

    修改页面

--- src/api/GaoAPI.js
+++ src/api/GaoAPI.js
@@ -1,1 +1,30 @@
+var host = "http://www.phptrain.cn/testadmin"
+// 登录API
 export const loginApi = "http://www.phptrain.cn/admin/unauth/account/login"
+
+// ---------------------首页API开始--------------------------------------------
+// 首页兼职总人数
+export const TotalPeopleAPI = "http://www.phptrain.cn/admin/home/countPartTimeTotalPeople"
+
+// 完成任务统计数API
+export const countComplateTaskAPI = "http://www.phptrain.cn/admin/home/countComplateTask"
+
+// 获取总任务数
+export const countTotalTaskAPI = "http://www.phptrain.cn/admin/home/countTotalTask"
+
+// ---------------------首页API结束--------------------------------------------
+
+// --------------------- 统计页面API开始 --------------------------------------
+export const statisticUserProfilePage= host+"/report/getUserProfilePage"
+// --------------------- 统计页面API结束 --------------------------------------
+
+
+// --------------------- 系统管理之权限管理 API开始 --------------------------------------
+// 获取权限管理内容
+export const getResourcePageAPI = host +"/admin/resource/getResourcePage"
+
+export const addResourceAPI = host + "/admin/resource/addResource"
+// 角色管理内容
+export const getRolePageAPI = host + "/admin/role/getRolePage"
+// --------------------- 系统管理之权限管理 API结束 --------------------------------------
+// --------------------- 统计页面API开始 --------------------------------------
--- src/components/page/SystemManage.vue
+++ src/components/page/SystemManage.vue
@@ -10,7 +10,7 @@
 									<el-button type="primary">查询</el-button>
 									<el-button type="warning">重置</el-button>
 								</div>
-								<el-button type="success" style="width:100px">新增按钮</el-button>
+								<el-button type="success" style="width:100px" @click="addQuanXianPanel = true">新增按钮</el-button>
 						</div>
 						<div class="quanxian-table">
 							  <el-table
@@ -38,6 +38,34 @@
 								</div>
 						
 						</div>
+						<el-dialog title="新增" :visible.sync="addQuanXianPanel">
+							  <div class="flexd">
+									权限编码： <el-input placeholder="请输入内容" style="width:60%;"></el-input>
+								</div> 
+								<div class="flexd">
+									权限名称： <el-input placeholder="请输入内容" style="width:60%;"></el-input>
+								</div> 
+								<div class="flexd">
+									描 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	述： <el-input type="textarea" :rows="2"  placeholder="请输入内容" style="width:60%;"></el-input>
+								</div> 
+								<div class="flexd">
+									隶属模块： 	<el-tree
+											:data="models"
+											show-checkbox
+											default-expand-all
+											node-key="id"
+											ref="tree"
+											highlight-current
+											style="width:60%;">
+										</el-tree>
+								</div> 
+									
+								</el-form-item>
+							  <div slot="footer" class="dialog-footer">
+									<el-button @click="addQuanXianPanel = false">取 消</el-button>
+									<el-button type="primary" @click="quanxianAdd">确 定</el-button>
+								</div>
+						</el-dialog>
 				</el-tab-pane>
 
 				<!-- 角色管理部分 -->
@@ -184,56 +212,18 @@
  </div>
 </template>
 <script>
+import {getResourcePageAPI,getRolePageAPI,addResourceAPI} from "@/api/GaoAPI"
  export default {
 	  data(){
 			return {
+				formLabelWidth:"120px",
 				 activeName: 'first',
 				 tableData: [{
           date: '2016-05-02',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        } ,{
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-				}
-				 ,{
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }],
+				}],
+				form:{},
 				options: [{
           value: '选项1',
           label: '全部'
@@ -246,14 +236,76 @@
         }],
 				optionsValue:"",
 			  begindata:'',
-				enddata:''
+				enddata:'',
+				addQuanXianPanel:false,
+				models:[{
+          id: 1,
+          label: '一级 1',
+          children: [{
+            id: 4,
+            label: '二级 1-1',
+            children: [{
+              id: 9,
+              label: '三级 1-1-1'
+            }, {
+              id: 10,
+              label: '三级 1-1-2'
+            }]
+          }]
+        }]
 			}
 		},
 		methods: {
       handleClick(tab, event) {
-        console.log(tab, event);
-      }
-    }
+				console.log(tab, event);
+				if (tab.label == "角色管理"){
+					 // 发送角色管理请求
+					 this.getRolePage(1)
+				}
+			},
+			// 权限部分-------------
+			getResourcePage(pageIndex,pageSize){
+				 if(!pageSize){
+					  var pageSize = 10
+				 }
+				 let params = new FormData()
+				 params.append("pageIndex",pageIndex)
+				 params.append("pageSize",pageSize)
+				 this.$http.post(getResourcePageAPI,params).then(res=>{
+					 console.log(res)
+				 })
+			},
+			// 添加权限
+			addResourceAPI(){
+				 let params = {
+						id:"11",
+						name:"22",
+				 }
+				 this.$http.post(addResourceAPI,params).then(res=>{
+					  console.log(res)
+				 })
+			},
+			quanxianAdd(){
+				 this.addQuanXianPanel = false
+				 alert("确定添加！")
+				//  console.log(this.$refs.tree.getCheckedNodes());
+				 this.addResourceAPI()
+			},
+			getRolePage(pageIndex,pageSize){
+				 if(!pageSize){
+					  var pageSize = 10
+				 }
+				 let params = new FormData()
+				 params.append("pageIndex",pageIndex)
+				 params.append("pageSize",pageSize)
+				 this.$http.post(getRolePageAPI,params).then(res=>{
+					 console.log(res)
+				 })
+			}
+		},
+		created() {
+			 this.getResourcePage(1)
+		}
  }
 </script>	
 <style lang="less" scoped>
@@ -277,6 +329,12 @@
 	 height: 36px;
 	 align-content: center;
 	 justify-content: space-between;
+}
+.flexd{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 10px;
 }
 
 </style>
